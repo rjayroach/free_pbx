82392b7b38e96a76e87f7da08501d87155bd8572
Robert Roach <rjayroach@gmail.com>
2016-04-03 19:20:40 +0800

    start cleaningf up the smells

--- app/models/free_pbx/asterisk_monitor.rb
+++ app/models/free_pbx/asterisk_monitor.rb
@@ -10,15 +10,30 @@ module FreePbx
 
     # Fix for Strong parameters
     attr_accessible :path, :uniqueid
-  
-    belongs_to :cdr, :class_name => "AsteriskCdr", :primary_key => 'uniqueid', :foreign_key => 'uniqueid'
 
-    validates :path, :uniqueid, :presence => true
+    belongs_to :cdr, class_name: "AsteriskCdr", primary_key: 'uniqueid', foreign_key: 'uniqueid'
 
+    validates :path, :uniqueid, presence: true
 
-    # 
+    # Build an index of recordings based on files in the file system
+    def self.reindex
+      Rails.logger.info "Begin reindex of monitors at #{Time.now}"
+      records = Array.new
+      create_time = Time.now
+      self.delete_all
+      self.file_list.each do |path| # /var/spool/asterisk/monitor/OUT-1438-1223171759.1400.wav
+        next unless (m = path.match(/(?:.*?-){2}(.*)/))
+        uniqueid = m[1].gsub('.wav', '')  # '1223171759.1400'
+        records.push "('#{path}', '#{uniqueid}', '#{create_time}', '#{create_time}')"
+        next unless records.size == PROCESS_RECORDS_SIZE
+        insert(records)
+        records = Array.new
+      end
+      insert(records) if records.size > 0 # If odd number of records exist then add them
+      Rails.logger.info "Finished reindex of monitors at #{Time.now}"
+    end
+
     # Return an Enumerator of files on the file system (recursive)
-    #
     def self.file_list
       require 'find'
       dirs = [ '/var/spool/asterisk/monitor' ]
@@ -26,45 +41,11 @@ module FreePbx
         Find.find(dirs)
       #end
     end
-  
 
-    #
-    # Build an index of recordings based on files in the file system
-    #
-    def self.reindex
-      Rails.logger.info "Begin reindex of monitors at #{Time.now}"
-      records = Array.new
-      create_time = Time.now
-      self.delete_all
-      self.file_list.each do |path|
-        r = path.reverse  # "vaw.0041.9571713221-8341-TUO/rotinom/ksiretsa/loops/rav/"
-        i = r.index('-')  # 19
-        if not i.nil?
-          uniqueid = r[4,i-4].reverse  # "1223171759.1400"
-          records.push "('#{path}', '#{uniqueid}', '#{create_time}', '#{create_time}')"
-          if records.size == PROCESS_RECORDS_SIZE
-            self.insert records
-            records = Array.new
-          end
-        end
-      end
-      self.insert(records) if records.size > 0 # If odd number of records exist then add them
-      Rails.logger.info "Finished reindex of monitors at #{Time.now}"
+    def self.insert(records)
+      # Quotes around field names do not work with mysql
+      sql = "INSERT INTO #{table_name} (path, uniqueid, created_at, updated_at) VALUES #{records.join(", ")}"
+      ActiveRecord::Base.connection.execute(sql)
     end
-
-
-    # 
-    # Execute a bulk SQL insert
-    #
-    def self.insert records
-      # quotes around field names do not work with mysql
-      #sql = "INSERT INTO #{self.table_name} ('path', 'uniqueid', 'created_at', 'updated_at') VALUES #{records.join(", ")}"
-      sql = "INSERT INTO #{self.table_name} (path, uniqueid, created_at, updated_at) VALUES #{records.join(", ")}"
-      ActiveRecord::Base.connection.execute sql
-    end
-
-
   end
 end
-
-
